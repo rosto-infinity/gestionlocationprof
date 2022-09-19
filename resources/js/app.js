9dc58696ce5a5c880b16cc96a59bdf771746dada
rosto-infinity <rostoinfocus@gmail.com>
2022-09-19 05:35:05 -1100

    install admin-lte

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
 require('./bootstrap');
+
+import "admin-lte/plugins/jquery/jquery";
+import "admin-lte/plugins/bootstrap/js/bootstrap.bundle";
+import "admin-lte/dist/js/adminlte";
+
+
+
+
+
+
+
+
+// <script src="plugins/jquery/jquery.min.js"></script>
+
+// <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+// <script src="dist/js/adminlte.min.js?v=3.2.0"></script>
